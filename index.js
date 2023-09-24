b05474f5ed8f1d96da2273859043b336ed13861c
Hanna Negash <hnegash.n@gmail.com>
2023-09-23 19:22:04 -0700

    Add comments

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//Match with lowercase of drivers' name
 const findMatching = (name, mini) =>
   name.filter((x) => x.toLowerCase() === mini.toLowerCase());
 
+//Returns all drivers whose names begin with the provided letters
 const fuzzyMatch = (name, letters) =>
-  name.filter((x) => x.toLowerCase().indexOf(letters.toLowerCase()) === 0);
+  name.filter((x) => x.indexOf(letters) === 0);
 
+//Returns each element whose name property matches the provided string argument
 const matchName = (name, nameMatch) => name.filter((x) => x.name === nameMatch);

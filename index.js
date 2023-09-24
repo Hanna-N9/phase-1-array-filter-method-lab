33ffa1d6fa855b8726ef101c6aedfb924e37d7f5
Hanna Negash <hnegash.n@gmail.com>
2023-09-23 19:18:44 -0700

    Use filter() to return return results

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution here
+const findMatching = (name, mini) =>
+  name.filter((x) => x.toLowerCase() === mini.toLowerCase());
+
+const fuzzyMatch = (name, letters) =>
+  name.filter((x) => x.toLowerCase().indexOf(letters.toLowerCase()) === 0);
+
+const matchName = (name, nameMatch) => name.filter((x) => x.name === nameMatch);
